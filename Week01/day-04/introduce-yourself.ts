80169f0cbe61d005712be3cba3a563e492692c49
Alexandra Rujzam <szappi04@gmail.com>
2018-02-15 11:43:28 +0100

    introducing myself

---
+++ Week01/day-04/introduce-yourself.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+/* Write a program that prints a few details to the console about you
+It should print each detail to a new line. */
+
+console.log("Rujzam Alexandra");
+console.log(27);
+console.log(1.60);
