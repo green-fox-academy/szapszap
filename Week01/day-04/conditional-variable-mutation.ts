640662261812bcf8e0d54f6bd436864fbe1ee307
Alexandra Rujzam <szappi04@gmail.com>
2018-02-15 17:24:11 +0100

    final condvarmut

--- Week01/day-04/conditional-variable-mutation.ts
+++ Week01/day-04/conditional-variable-mutation.ts
@@ -43,3 +43,23 @@ if (credits >= 50 && !isBonus) {
 } else {
     console.log(c);
 }
+
+let d: number =  8;
+let time: number = 120;
+let out3: string = '';
+// if d is dividable by 4
+// and time is not more than 200
+// set out3 to "check"
+// if time is more than 200
+// set out3 to "Time out"
+// otherwise set out3 to "Run Forest Run!"
+if ((d % 4) === 0 && time <= 200) {
+    out3 = "check"
+    console.log(out3);
+} else if (time > 200) {
+    out3 = "Time out"
+    console.log(out3); 
+} else {
+    out3 = "Run Forest Run!"
+    console.log(out3);
+}
