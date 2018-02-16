e44f82ac082d9792ba5e682ee9afd52d1dce93d4
Alexandra Rujzam <szappi04@gmail.com>
2018-02-16 11:30:42 +0100

    triangle

---
+++ Week01/day-05/draw-triangle.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Write a program that draws a triangle
+
+let lineCount: number = 4;
+let star: string = '*';
+for (let i: number = 1; i <= lineCount; i++) {
+    for (let j: number = 1; j <= 1; j++)
+    console.log(star);
+    star += '*';
+}
