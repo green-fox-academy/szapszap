e1b8af7a965afcdb93f2bf72e41fcf7dc55677b9
Alexandra Rujzam <szappi04@gmail.com>
2018-02-16 10:05:25 +0100

    print evens

---
+++ Week01/day-05/print-even.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+let a: number = 0;
+while (a <= 500) {
+    if ((a % 2) === 0) {
+        console.log(a);
+    } a++;
+}
