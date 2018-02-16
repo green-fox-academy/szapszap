9b3261daf800ab787b44933dbb4a587a481ea13e
Alexandra Rujzam <szappi04@gmail.com>
2018-02-16 09:42:51 +0100

    today's first commit

---
+++ Week01/day-05/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let j: number = 0; j < 100; j++) {
+    console.log("I won't cheat on the exam!");
+}
