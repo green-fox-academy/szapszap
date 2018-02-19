ecfb5e837a8bdf271d642d862eec49b4e06939a1
Alexandra Rujzam <szappi04@gmail.com>
2018-02-19 17:27:27 +0100

    change-el.

---
+++ Week02/day-01/change-element.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 
+// -  Print the fourth element as a test
+
+let numList = [1, 2, 3, 8, 5, 6];
+numList[3] = 4;
+console.log(numList[3]);
