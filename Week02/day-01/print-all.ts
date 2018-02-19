a1516ab53859d7d25f172d7fecb8a3f1ae8823c5
Alexandra Rujzam <szappi04@gmail.com>
2018-02-19 16:48:05 +0100

    print-all

---
+++ Week02/day-01/print-all.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+// -  Create a variable named `numList` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `numList` to the console*
+// *hint: use a loop, console.log(numList) won't cut it
+// -  bonus for using the correct built in array method
+
+let numList = [4, 5, 6, 7];
+numList.forEach(function(element) {
+    console.log(element);
+});
