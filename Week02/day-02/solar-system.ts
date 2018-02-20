cd7345632e8cff06ffacfec9b940544c469f6d2b
Alexandra Rujzam <szappi04@gmail.com>
2018-02-20 15:05:41 +0100

    solar

--- Week02/day-02/solar-system.ts
+++ Week02/day-02/solar-system.ts
@@ -5,6 +5,11 @@
 // bonus for using some built in methods
 let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
 // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
+function putSaturn(item: string[]) {
+    item = item.concat("Saturn");
+    return item;
+}
+
 console.log(putSaturn(planetList));
 
-export = putSaturn;
