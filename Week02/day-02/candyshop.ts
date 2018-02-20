69a9e001d7395403ce37077edc5d589972354acd
Alexandra Rujzam <szappi04@gmail.com>
2018-02-20 17:46:40 +0100

    candy

---
+++ Week02/day-02/candyshop.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Accidentally we added "2" and "false" to the array.
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+// Create a function called sweets() which takes the list as a parameter.
+// Expected output: "Cupcake", "Croissant", "Brownie", "Ice cream"
+
+let shopItems: any[] = ['Cupcake', 2, 'Brownie', false];
+
+function sweets(item: any[]) {
+    let cookies: string[];
+    
+}
