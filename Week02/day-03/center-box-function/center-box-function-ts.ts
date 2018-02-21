9e80932cc169af047bdf1bf5a20dfd2d14a9bee3
Alexandra Rujzam <szappi04@gmail.com>
2018-02-21 16:02:13 +0100

    3 center boxes

---
+++ Week02/day-03/center-box-function/center-box-function-ts.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 1 parameter:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+function square(x) {
+    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    ctx.fillRect(canvas.width / 2 - x / 2, canvas.height / 2 - x / 2, x, x);
+}
+
+let a: number = 180;
+
+for (let i = 0; i < 3; i++) {
+    square(a);
+    a -= 50;
+};
+
