99c1946946b1b3a0aceb26893e5e8b0298f483ff
Alexandra Rujzam <szappi04@gmail.com>
2018-02-22 14:10:49 +0100

    rainbow' git commit -m rainbow

--- Week02/day-04/rainbow-box-function/rainbow-box-function-ts.ts
+++ Week02/day-04/rainbow-box-function/rainbow-box-function-ts.ts
@@ -8,3 +8,33 @@ const ctx = canvas.getContext('2d');
 // The square size, and the fill color,
 // and draws a square of that size and color to the center of the canvas.
 // Create a loop that fills the canvas with rainbow colored squares.
+// let a: number = 20;
+
+
+let x: 250;
+let color: string;
+
+function drawSquares(x: number, color: string) {
+    ctx.fillStyle = color;
+    ctx.fillRect(canvas.width / 2 - x / 2, canvas.height / 2 - x / 2, x, x);  
+}
+
+let rainbow = {
+    1: '#FF0000',
+    2: '#FF0066',
+    3: '#CD00FF',
+    4: '#2200FF',
+    5: '#0089FF',
+    6: '#00FF80',
+    7: '#5EFF00',
+    8: '#EFFF00',
+    9: '#FF8900',
+    10: '#FF3C00',
+    11: '#FF0000',
+}
+
+ for (let i = 1; i <= 11; i++) {
+     x -= 20;
+     color = rainbow[i];
+     drawSquares(x, color);
+ };
