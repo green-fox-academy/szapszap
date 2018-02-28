403cd32f8aa78f13af4b2568c0a38eb0c992e32e
Alexandra Rujzam <szappi04@gmail.com>
2018-02-28 18:11:21 +0100

    lightning talk

---
+++ Week03/day-03/fleet.ts
@@ -0,0 +1,14 @@
+'use strict'
+
+import {Thing} from "./thing";
+
+class Fleet {
+    private things: Thing[];
+
+    constructor(){}
+    add(thing: Thing){
+        this.things.push(thing);
+    }
+}
+
+export {Fleet};
---
+++ Week03/day-03/thing.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+class Thing {
+    private name: string;
+    private completed: boolean;
+
+    constructor(name: string){
+        this.name = name;
+    }
+    public complete() {
+        this.completed = true;
+    }
+}
+
+export {Thing};
