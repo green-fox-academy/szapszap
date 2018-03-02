6dd009e9a90295fda4f54396462e2ea18cf777c2
Alexandra Rujzam <szappi04@gmail.com>
2018-03-02 13:26:38 +0100

    demo

--- Week03/day-04/centered-square-ts.ts
+++ Week03/day-04/centered-square-ts.ts
@@ -10,20 +10,23 @@ let radius: number = 200;
 let x: number = 200;
 let y: number = 200;
 let p: number = 0;
+let red: number = 40; 
+let green: number = 20;
 
 function circles(x, y, radius, p) {
     if (radius > 10) {
         ctx.beginPath();
+        ctx.strokeStyle = 'rgba(' + red + ',' + green + ', 125)';
         ctx.arc(x, y, radius, 0, Math.PI * 2);
         ctx.stroke();
         radius /= 2;
-
+        red += 2;
+        green += 1;
         circles(x + radius / 1.15, y + radius / 2, radius, p);
         circles(x - radius / 1.15, y + radius / 2, radius, p);
         circles(x, y-radius, radius, p);
     }
 }
-
 circles(x, y, radius, p);
 
 
