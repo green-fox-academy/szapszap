93b8984380c0d24f4e5bae5a4e7dfcdb5f0d8f2d
Alexandra Rujzam <szappi04@gmail.com>
2018-03-02 13:07:48 +0100

    apollonian fractals

--- Week03/day-04/centered-square-ts.ts
+++ Week03/day-04/centered-square-ts.ts
@@ -6,21 +6,24 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 // Draw a green 10x10 square to the center of the canvas.
 
-let size: number = 400;
-let x: number = 0;
-let y: number = 0;
-ctx.strokeStyle = 'green';
-ctx.strokeRect(x, y, size, size);
-let subSize = size/3;
+let radius: number = 200;
+let x: number = 200;
+let y: number = 200;
+let p: number = 0;
 
-function drawSquare(x, y, subSize) {
-    x = x + subSize;
-    y = y + subSize;
-    ctx.strokeStyle = 'green';
-    ctx.strokeRect(x, y, subSize, subSize);
+function circles(x, y, radius, p) {
+    if (radius > 10) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.stroke();
+        radius /= 2;
+
+        circles(x + radius / 1.15, y + radius / 2, radius, p);
+        circles(x - radius / 1.15, y + radius / 2, radius, p);
+        circles(x, y-radius, radius, p);
+    }
 }
 
-drawSquare
-
+circles(x, y, radius, p);
 
 
