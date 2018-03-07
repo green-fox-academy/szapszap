d4472450cbfd9331acb7fa185cb7e17f617dc2d2
Alexandra Rujzam <szappi04@gmail.com>
2018-03-07 17:47:59 +0100

    reservations

---
+++ Week04/day-03/reservations.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+interface Reservations {
+    getDowBooking(): string;
+    getCodeBooking(): string;
+  };
+
+let daysOfTheWeek: string[] = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+
+class Reservations {
+    getDowBooking() {
+        let randomIndex: number = Math.floor(Math.random() * daysOfTheWeek.length);
+        return daysOfTheWeek[randomIndex];
+    }
+    getCodeBooking() {
+        let text: string = "";
+        let possible = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (let i = 0; i < 8; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+            if (text.length === 8) {
+            return text;
+        }
+    }
+   } 
+} 
+  let reservations = new Reservations;
+
+  console.log("Booking# " + reservations.getCodeBooking() + " for " + reservations.getDowBooking());
+  console.log("Booking# " + reservations.getCodeBooking() + " for " + reservations.getDowBooking());
+  console.log("Booking# " + reservations.getCodeBooking() + " for " + reservations.getDowBooking());
+  console.log("Booking# " + reservations.getCodeBooking() + " for " + reservations.getDowBooking());
