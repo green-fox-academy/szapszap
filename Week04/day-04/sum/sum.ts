3dbc0ee36f7162302e446ef44bd55797ea63cb2e
Alexandra Rujzam <szappi04@gmail.com>
2018-03-08 15:09:43 +0100

    sum

--- Week04/day-04/sum/sum.ts
+++ Week04/day-04/sum/sum.ts
@@ -6,14 +6,12 @@ export class sumIt {
         for(let i:number = 0; i < sum.length; i++) {
             if (typeof sum[i] === 'string') {
                 return 0;
-                } 
-            }   
-            try {
-    let sum2 = sum.reduce((a, b) => a + b, 0);
-    return sum2;
-    } catch(err) {
-        return 0;
-    }
-       
-    }
+                 } 
+            } try {
+                let sum2 = sum.reduce((a, b) => a + b, 0);
+                return sum2;
+            } catch(err) {
+                return 0;
+            }    
+        }
 }
