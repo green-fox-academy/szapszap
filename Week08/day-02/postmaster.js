00c2f4a0568f88b93c13173ff510e2dc7b9f5f09
Alexandra Rujzam <szappi04@gmail.com>
2018-04-10 13:35:09 +0200

    postmaster

---
+++ Week08/day-02/postmaster.js
@@ -0,0 +1,24 @@
+// Write the logic of the postMaster function. It shoud have two parameters: 
+//  - poBox: a string that identifies the sender
+//  - dispatchFunction: a function that will be called with the message
+//
+// The postMaster function should return a function that takes two arguments:
+//  - targetBox: a string that identifies the po_box
+//  - message: a string that contains the message
+// If the poBox matches the targetBox only that case call the dispatchFunction
+
+let postMaster = (poBox, dispatchFunction) => {
+  return function(targetBox, message) {
+    if (poBox === targetBox) {
+      dispatchFunction(message);
+    }
+  } 
+}
+
+function myAction(message) {
+  console.log('The message says:', message);
+}
+
+const myPrivateBox = postMaster('super_secret_box', myAction);
+myPrivateBox('super_secret_box', 'The eagle has landed!'); 
+myPrivateBox('false_alarm', 'The eagle has landed! Heeeeloooooo!');
