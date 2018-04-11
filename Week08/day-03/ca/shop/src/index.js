09ac37af7c8e157660457d65be3f9e5c7ff183d7
Alexandra Rujzam <szappi04@gmail.com>
2018-04-11 19:56:27 +0200

    final

--- Week08/day-03/ca/shop/src/index.js
+++ Week08/day-03/ca/shop/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 const mysql = require('mysql');
@@ -15,9 +15,25 @@ const conn = mysql.createConnection({
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'))
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+const types = {
+  entertainment: 1,
+  groceries: 2,
+  restaurant: 3,
+  services: 4,
+  transport: 5,
+};
+
 app.get('/', (req, res) => {
-  const sql = 'SELECT * FROM history ORDER BY date DESC LIMIT 15;';
-  conn.query(sql, (err, result) => {
+  let sql = 'SELECT * FROM history ORDER BY date DESC LIMIT 15;';
+  let queryInputs = [];
+
+  if (req.query.type !== undefined) {
+    sql = 'SELECT * FROM history WHERE type = ? LIMIT 25;';
+    queryInputs = [types[req.query.type]];
+  }
+  conn.query(sql, queryInputs, (err, result) => {
     if (err) {
       console.log(err);
       res.sendStatus(500);
