6d418865ee82ac586fe9705be8af860054894489
Alexandra Rujzam <szappi04@gmail.com>
2018-04-11 19:35:42 +0200

    sql + css

--- Week08/day-03/ca/shop/src/index.js
+++ Week08/day-03/ca/shop/src/index.js
@@ -2,14 +2,31 @@ const express = require('express')
 // const path = require('path');
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'szapszap',
+  password: 'Seiyastar*0',
+  database: 'shop',
+  port: 3306,
+})
 
 app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Dashboard',
-  });
+  const sql = 'SELECT * FROM history ORDER BY date DESC LIMIT 15;';
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.render('index', {
+      title: 'Dashboard',
+      history: result,
+    });
+  })
 });
 
 app.listen(PORT, () => {
